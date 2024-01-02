4fce4a3c5b03985508f318e55bf1492d601d5de1
Rasoulmadani <ali.rahbary@chmail.ir>
2024-01-02 09:10:15 +0330

    send project to server and create controller for admin and agent

---
+++ resources/views/admin/admin_dashboard.blade.php
@@ -0,0 +1,19 @@
+<!--
+/* ﷲ  ﷽  ﷲ  ﷽   ﷽  ﷲ  ﷽  ﷲ  ﷽  ﷲ  ﷽  ﷲ  ﷽  ﷲ.. 
+|:          **** بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيمِ ****           :|
+|:     ⚡☝..✍-☝..✍-☝..✍-☝..✍-☝..✍-☝..✍⚡   :|
+|:               ⌚ in the name of allah ⌛              :|
+﷽﷽﷽﷽﷽﷽﷽﷽﷽﷽﷽﷽﷽﷽﷽﷽﷽﷽﷽﷽﷽*/
+-->
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="" />
+        <title>Admin Dashboard</title>
+    </head>
+    <body>
+        <h1>Admin Dashboard</h1>
+    </body>
+</html>
